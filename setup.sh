376d5a08644d34429cddbcb410c019cbd7f4706b
John Harrington <harrington.john@husky.neu.edu>
2020-08-17 23:34:29 -0700

    Fixed bugs, should be ready for test deployment

--- setup.sh
+++ setup.sh
@@ -1,24 +1,35 @@
 cd ..
 git clone git@github.com:Politik-ai/data_collection.git
+cd data_collection
+pip install -r requirements.txt
+cd ..
 git clone git@github.com:unitedstates/congress.git
+cd congress
+pip install -r requirements.txt
+cd ..
 git clone git@github.com:Politik-ai/political_analytics.git
+cd political_analytics
+pip install -r requirements.txt
+cd ../fullstack
 
 while [ "$1" != "" ]
 do
-    if [ "$1" == "--dev" ]
+    if [ "$1" == "--test" ]
     then
-        cd congress
-        pip install -r requirements.txt
-
+        cd ../congress/
+        (echo "import os, sys"; 
+            echo "sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tasks'))"; 
+            echo "from utils import require_congress_legislators_repo"; 
+            echo "require_congress_legislators_repo()") | python
         cd ../fullstack
-        ./test_setup.sh
+        ./test_data_setup.sh
 
-        cd ../data_collection
-        pip3 install -r requirements.txt
-        cd database_filler
+        cd ../data_collection/database_filler
         python3 update.py
+        cd ../../fullstack
     fi
 shift
+echo "Test"
 done
 
 
--- test_data_setup.sh
+++ test_data_setup.sh
@@ -1,7 +1,8 @@
 cd ../congress
 
 
+
 ./run govinfo --bulkdata=BILLSTATUS --congress=113
 ./run bills --congress=113
 ./run govinfo --collections=BILLS --congress=113 --store=text --extract=text
-#./run votes --congress=113 --session=2014
+./run votes --congress=113 --session=2014
