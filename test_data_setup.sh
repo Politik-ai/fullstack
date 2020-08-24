f093a5f86c91be3d194a86e1262c7ec4f6724c6f
John Harrington <harrington.john@husky.neu.edu>
2020-08-23 22:10:43 -0700

    Changed test congress to current 116 for cron job testing

--- test_data_setup.sh
+++ test_data_setup.sh
@@ -2,7 +2,7 @@ cd ../congress
 
 
 
-./run govinfo --bulkdata=BILLSTATUS --congress=113
-./run bills --congress=113
-./run govinfo --collections=BILLS --congress=113 --store=text --extract=text
-./run votes --congress=113 --session=2014
+./run govinfo --bulkdata=BILLSTATUS --congress=116
+./run bills --congress=116
+./run govinfo --collections=BILLS --congress=116 --store=text --extract=text
+./run votes --congress=116 --session=2020
